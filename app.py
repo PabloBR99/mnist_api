ea36be5073bd544003eb0e8947c5c381519ac9d2
PabloBR99 <50883384+PabloBR99@users.noreply.github.com>
2023-06-19 02:55:11 +0200

    added zoom

--- app.py
+++ app.py
@@ -5,10 +5,54 @@ from flask_cors import CORS
 from tensorflow import keras
 import json
 import scipy as sp
-import cv2
+from scipy.ndimage import zoom
 
-def recenter(arr2):
-    arr = cv2.copyMakeBorder(arr2, 10, 10, 10, 10, cv2.BORDER_REPLICATE)
+
+def clipped_zoom(img, zoom_factor, **kwargs):
+
+    h, w = img.shape[:2]
+
+    # For multichannel images we don't want to apply the zoom factor to the RGB
+    # dimension, so instead we create a tuple of zoom factors, one per array
+    # dimension, with 1's for any trailing dimensions after the width and height.
+    zoom_tuple = (zoom_factor,) * 2 + (1,) * (img.ndim - 2)
+
+    # Zooming out
+    if zoom_factor < 1:
+
+        # Bounding box of the zoomed-out image within the output array
+        zh = int(np.round(h * zoom_factor))
+        zw = int(np.round(w * zoom_factor))
+        top = (h - zh) // 2
+        left = (w - zw) // 2
+
+        # Zero-padding
+        out = np.zeros_like(img)
+        out[top:top+zh, left:left+zw] = zoom(img, zoom_tuple, **kwargs)
+
+    # Zooming in
+    elif zoom_factor > 1:
+
+        # Bounding box of the zoomed-in region within the input array
+        zh = int(np.round(h / zoom_factor))
+        zw = int(np.round(w / zoom_factor))
+        top = (h - zh) // 2
+        left = (w - zw) // 2
+
+        out = zoom(img[top:top+zh, left:left+zw], zoom_tuple, **kwargs)
+
+        # `out` might still be slightly larger than `img` due to rounding, so
+        # trim off any extra pixels at the edges
+        trim_top = ((out.shape[0] - h) // 2)
+        trim_left = ((out.shape[1] - w) // 2)
+        out = out[trim_top:trim_top+h, trim_left:trim_left+w]
+
+    # If zoom_factor == 1, just return the input array
+    else:
+        out = img
+    return out
+
+def recenter(arr):
     slicing = sp.ndimage.find_objects(arr != 0, max_label=1)[0]
     center_slicing = tuple(
         slice((dim - sl.stop + sl.start) // 2, (dim + sl.stop - sl.start) // 2)
@@ -29,9 +73,10 @@ def predict():
     # pixels_c = recenter(np.reshape(req, (28, 28)))
     pixels_c = recenter(np.array(req))
     pixels = np.reshape(pixels_c, (1, 28, 28, 1))
+    pixels_f = clipped_zoom(pixels, 1.1)
     if pixels is None:
        abort(400)
-    prediction = model.predict([pixels])
+    prediction = model.predict([pixels_f])
     res = prediction[0].tolist()
     print('pred:', res)
     return {"result": res}
