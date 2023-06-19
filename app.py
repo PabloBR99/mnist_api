9e918d3bb37c6cb32973c4a6df5439183c4c0b77
PabloBR99 <50883384+PabloBR99@users.noreply.github.com>
2023-06-19 04:16:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def predict():
     req = request.get_json()['pixels']
     # pixels_c = recenter(np.reshape(req, (28, 28)))
     pixels = np.array(req)
-    pixels_f = np.reshape(pixels_f, (1, 28, 28, 1))
+    pixels_f = np.reshape(pixels, (1, 28, 28, 1))
     # pixels_f = clipped_zoom(pixels_c, 1.1)
     if pixels_f is None:
        abort(400)
